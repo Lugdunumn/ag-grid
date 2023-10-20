6006006ed53a46f0af070eb187a844be6e40b160
Bernie Sumption <bernie@berniecode.com>
2023-10-20 23:55:51 +0100

    WIP

--- grid-packages/ag-grid-theme-builder/src/design-system/render.test.ts
+++ grid-packages/ag-grid-theme-builder/src/design-system/render.test.ts
@@ -1,36 +1,150 @@
 import { expect, test } from 'vitest';
-import { parseSelectorString } from './render';
+import { cartesianProduct, parseSelectorsString, renderCssRuleSet } from './render';
 
-// test(renderCssRuleSet, () => {
-//   expect(
-//     renderCssRuleSet({
-//       foo: {
-//         color: 'red',
+test('flattening nested rules', () => {
+  expect(
+    renderCssRuleSet({
+      foo: {
+        color: 'red',
 
-//         bar: {
-//           color: 'green',
-//         },
-//       },
-//     }),
-//   ).toMatchInlineSnapshot('""');
-// });
+        bar: {
+          color: 'green',
+        },
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+    ".ag-foo {
+    	color: red;
+    }
+     .ag-foo .ag-bar {
+    	color: green;
+    }"
+  `);
+});
 
-test(parseSelectorString, () => {
-  expect(parseSelectorString('foo')).toEqual([['.ag-foo']]);
-  expect(parseSelectorString('fooBar')).toEqual([['.ag-foo-bar']]);
-  expect(parseSelectorString('foo bar')).toEqual([['.ag-foo', '.ag-bar']]);
-  expect(parseSelectorString('foo, bar')).toEqual([['.ag-foo'], ['.ag-bar']]);
+test('expanding nested rules', () => {
+  expect(
+    renderCssRuleSet({
+      'foo, bar': {
+        'baz, quux': {
+          color: 'green',
+        },
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+    ".ag-foo .ag-baz,
+     .ag-foo .ag-quux,
+     .ag-bar .ag-baz,
+     .ag-bar .ag-quux {
+    	color: green;
+    }"
+  `);
+});
+
+test('joining rules with &', () => {
+  expect(
+    renderCssRuleSet({
+      'foo, bar': {
+        '&-baz': {
+          color: 'red',
+        },
+        '&:not(quux)': {
+          color: 'green',
+        },
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+    ".ag-foo-baz,
+     .ag-bar-baz {
+    	color: red;
+    }
+     .ag-foo:not(.ag-quux),
+     .ag-bar:not(.ag-quux) {
+    	color: green;
+    }"
+  `);
+});
+
+test('RTL styles', () => {
+    Next up: fix this test case
+  expect(
+    renderCssRuleSet({
+      foo: {
+        paddingLeading: 4,
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+      ".ag-ltr .ag-foo {
+          padding-left: 4px;
+      }
+      .ag-rtl .ag-foo {
+          padding-right: 4px;
+      }"
+    `);
+});
+
+test(parseSelectorsString, () => {
+  expect(parseSelectorsString('foo')).toEqual(['.ag-foo']);
+  expect(parseSelectorsString('fooBar')).toEqual(['.ag-foo-bar']);
+  expect(parseSelectorsString('foo bar')).toEqual(['.ag-foo .ag-bar']);
+  expect(parseSelectorsString('foo, bar')).toEqual(['.ag-foo', '.ag-bar']);
 
   // append state classes
-  expect(parseSelectorString('foo.stateClass')).toEqual([['.ag-foo.ag-state-class']]);
-  expect(parseSelectorString('foo:not(stateClass)')).toEqual([['.ag-foo:not(.ag-state-class)']]);
+  expect(parseSelectorsString('foo.stateClass')).toEqual(['.ag-foo.ag-state-class']);
+  expect(parseSelectorsString('foo:not(stateClass)')).toEqual(['.ag-foo:not(.ag-state-class)']);
 
   // pseudo selectors
-  expect(parseSelectorString('foo:last-child')).toEqual([['.ag-foo:last-child']]);
+  expect(parseSelectorsString('foo:last-child')).toEqual(['.ag-foo:last-child']);
+
+  // attributes
+  expect(parseSelectorsString('[data-foo="bar"]')).toEqual(['[data-foo="bar"]']);
+  expect(parseSelectorsString('foo[data-foo="bar"]')).toEqual(['.ag-foo[data-foo="bar"]']);
 
   // pseudo classes
-  expect(parseSelectorString('foo::before')).toEqual([['.ag-foo::before']]);
+  expect(parseSelectorsString('foo::before')).toEqual(['.ag-foo::before']);
 
   // html elements
-  expect(parseSelectorString('input[type="button"]')).toEqual([['input[type="button"]']]);
+  expect(parseSelectorsString('input')).toEqual(['input']);
+  expect(parseSelectorsString('input[type="button"]')).toEqual(['input[type="button"]']);
+});
+
+test(cartesianProduct, () => {
+  expect(
+    cartesianProduct([
+      [1, 2],
+      [3, 4],
+    ]),
+  ).toEqual([
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+  ]);
+
+  expect(cartesianProduct([])).toEqual([[]]);
+  expect(cartesianProduct([[1]])).toEqual([[1]]);
+  expect(cartesianProduct([[1, 2]])).toEqual([[1], [2]]);
+  expect(cartesianProduct([[1, 2], [3]])).toEqual([
+    [1, 3],
+    [2, 3],
+  ]);
+  expect(
+    cartesianProduct([
+      [1, 2],
+      [3, 4],
+    ]),
+  ).toEqual([
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+  ]);
+  expect(cartesianProduct([[1, 2], [3], [4, 5, 6]])).toEqual([
+    [1, 3, 4],
+    [1, 3, 5],
+    [1, 3, 6],
+    [2, 3, 4],
+    [2, 3, 5],
+    [2, 3, 6],
+  ]);
 });
--- grid-packages/ag-grid-theme-builder/src/design-system/render.ts
+++ grid-packages/ag-grid-theme-builder/src/design-system/render.ts
@@ -1,4 +1,4 @@
-type CssValue = null | undefined | string;
+type CssValue = null | undefined | string | number;
 
 interface NestedRuleSet {
   [selector: string]: NestedDeclarations;
@@ -9,7 +9,7 @@ interface NestedDeclarations {
 }
 
 interface FlatRule {
-  selectors: string[][];
+  selectors: string[];
   declarations: [string, string][];
 }
 
@@ -25,41 +25,58 @@ const addRules = (rules: FlatRule[], result: string[]) => {
     if (declarations.length === 0 || selectors.length === 0) continue;
     result.push(selectors.join(',\n'), ' {\n');
     for (const [property, value] of declarations) {
-      result.push('\t', property, ': ', value, '\n');
+      result.push('\t', property, ': ', value, ';\n');
     }
     result.push('}\n');
   }
 };
 
+// Flattens a nested rule set e.g. {someClass: {anotherClass: {color: red}}}
+// flattens to .ag-some-class .ag-another-class { color: red; }
 const flattenCssRuleSet = (input: NestedRuleSet): FlatRule[] => {
   const result: FlatRule[] = [];
-  for (const [rawSelector, declarations] of Object.entries(input)) {
-    const selectors = parseSelectorString(rawSelector);
-    flattenCssRule(selectors, declarations, result);
+  for (const [selectorsString, declarations] of Object.entries(input)) {
+    flattenCssRule(selectorsString, declarations, [], result);
   }
   return result;
 };
 
-const flattenCssRule = (selectors: string[][], rule: NestedDeclarations, result: FlatRule[]) => {
-  const propertiesRule: FlatRule = {
-    selectors,
-    declarations: [],
-  };
+// flatten a block that can contain declarations e.g. 'color: red' or nested blocks
+const flattenCssRule = (
+  selectorsString: string,
+  rule: NestedDeclarations,
+  ancestorSelectors: string[][],
+  result: FlatRule[],
+) => {
+  const selectors = parseSelectorsString(selectorsString);
+  // if this block has declarations, emit them as a single css style rule block
+  const declarations: [string, string][] = [];
   for (const [key, value] of Object.entries(rule)) {
     if (value == null || typeof value === 'object') continue;
-    propertiesRule.declarations.push([toKebabCase(key), value]);
+    const renderedValue = typeof value === 'number' ? `${value}px` : value;
+    declarations.push([toKebabCase(key), renderedValue]);
   }
-  if (propertiesRule.declarations.length > 0) {
+  const selectorsPath: string[][] = [...ancestorSelectors, selectors];
+  if (declarations.length > 0) {
+    const propertiesRule: FlatRule = {
+      selectors: cartesianProduct(selectorsPath).map(joinSelectors),
+      declarations,
+    };
     result.push(propertiesRule);
+  }
+
+  // recurse into nested blocks
+  for (const [key, value] of Object.entries(rule)) {
+    if (value == null || typeof value !== 'object') continue;
+    flattenCssRule(key, value, selectorsPath, result);
   }
 };
 
-export const parseSelectorString = (rawSelector: string): string[][] =>
+export const parseSelectorsString = (rawSelector: string): string[] =>
   rawSelector
     .trim()
-    .replaceAll(/(?<![\w"'[:-])\w+|\.\w+/g, mapElementsToClassNames)
-    .split(/\s*,\s*/)
-    .map((part) => part.split(/\s+/));
+    .replaceAll(/(?<![\w"'[:-])[a-z]+|\.[a-z]+/gi, mapElementsToClassNames)
+    .split(/\s*,\s*/);
 
 const mapElementsToClassNames = (element: string): string => {
   if (knownElements.has(element)) return element;
@@ -69,9 +86,23 @@ const mapElementsToClassNames = (element: string): string => {
   return '.ag-' + toKebabCase(element);
 };
 
+// https://en.wikipedia.org/wiki/Cartesian_product
+export const cartesianProduct = <T>(input: T[][]): T[][] => {
+  const last = input[input.length - 1];
+  if (!last) return [[]];
+  const rest = input.slice(0, -1);
+  return cartesianProduct(rest).flatMap((c) => last.map((v) => [...c, v]));
+};
+
+const joinSelectors = (selectors: string[]) =>
+  selectors
+    .map((selector) => (selector.startsWith('&') ? selector.substring(1) : ' ' + selector))
+    .join('');
+
 const toKebabCase = (camelCase: string) =>
   camelCase.replaceAll(/(?<=[a-z])(?=[A-Z])/g, '-').toLowerCase();
 
+// TODO embed a much smaller set of elements that we actually use
 const knownElements = new Set(
   'a abbr address area article aside audio b base bdi bdo blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr'.split(
     ' ',
