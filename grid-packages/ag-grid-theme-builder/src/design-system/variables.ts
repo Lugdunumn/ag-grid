b22321eae7b29a88324729adebf20a419f46efd6
Bernie Sumption <bernie@berniecode.com>
2023-10-21 20:35:26 +0100

    WIP

---
+++ grid-packages/ag-grid-theme-builder/src/design-system/variables.ts
@@ -0,0 +1,124 @@
+type ColorVariable =
+  | 'activeColor'
+  | 'advancedFilterColumnPillColor'
+  | 'advancedFilterJoinPillColor'
+  | 'advancedFilterOptionPillColor'
+  | 'advancedFilterValuePillColor'
+  | 'backgroundColor'
+  | 'borderColor'
+  | 'checkboxBackgroundColor'
+  | 'checkboxCheckedColor'
+  | 'checkboxIndeterminateColor'
+  | 'checkboxUncheckedColor'
+  | 'chipBackgroundColor'
+  | 'columnHoverColor'
+  | 'controlPanelBackgroundColor'
+  | 'dataColor'
+  | 'disabledForegroundColor'
+  | 'foregroundColor'
+  | 'headerBackgroundColor'
+  | 'headerCellHoverBackgroundColor'
+  | 'headerCellMovingBackgroundColor'
+  | 'headerColumnResizeHandleColor'
+  | 'headerColumnSeparatorColor'
+  | 'headerForegroundColor'
+  | 'inputBorderColor'
+  | 'inputBorderColorInvalid'
+  | 'inputDisabledBackgroundColor'
+  | 'inputDisabledBorderColor'
+  | 'inputFocusBorderColor'
+  | 'invalidColor'
+  | 'materialPrimaryColor'
+  | 'minichartSelectedChartColor'
+  | 'minichartSelectedPageColor'
+  | 'modalOverlayBackgroundColor'
+  | 'oddRowBackgroundColor'
+  | 'rangeSelectionBackgroundColor'
+  | 'rangeSelectionBackgroundColor2'
+  | 'rangeSelectionBackgroundColor3'
+  | 'rangeSelectionBackgroundColor4'
+  | 'rangeSelectionBorderColor'
+  | 'rangeSelectionChartBackgroundColor'
+  | 'rangeSelectionChartCategoryBackgroundColor'
+  | 'rangeSelectionHighlightColor'
+  | 'rowBorderColor'
+  | 'rowHoverColor'
+  | 'secondaryBorderColor'
+  | 'secondaryForegroundColor'
+  | 'selectedRowBackgroundColor'
+  | 'selectedTabUnderlineColor'
+  | 'sideButtonSelectedBackgroundColor'
+  | 'subheaderBackgroundColor'
+  | 'subheaderToolbarBackgroundColor'
+  | 'toggleButtonOffBackgroundColor'
+  | 'toggleButtonOffBorderColor'
+  | 'toggleButtonOnBackgroundColor'
+  | 'toggleButtonOnBorderColor'
+  | 'toggleButtonSwitchBackgroundColor'
+  | 'toggleButtonSwitchBorderColor'
+  | 'tooltipBackgroundColor'
+  | 'valueChangeDeltaDownColor'
+  | 'valueChangeDeltaUpColor'
+  | 'valueChangeValueHighlightBackgroundColor';
+
+
+  Next up: classify these:
+
+  | "advancedFilterBuilderIndentSize"
+| "borderRadius"
+| "borders"
+| "bordersCritical"
+| "bordersInput"
+| "bordersInputInvalid"
+| "bordersSecondary"
+| "bordersSideButton"
+| "cardRadius"
+| "cardShadow"
+| "cellHorizontalBorder"
+| "cellHorizontalPadding"
+| "cellWidgetSpacing"
+| "checkboxBorderRadius"
+| "columnSelectIndentSize"
+| "filterToolPanelGroupIndent"
+| "fontFamily"
+| "fontSize"
+| "gridSize"
+| "headerColumnResizeHandleDisplay"
+| "headerColumnResizeHandleHeight"
+| "headerColumnResizeHandleWidth"
+| "headerColumnSeparatorDisplay"
+| "headerColumnSeparatorHeight"
+| "headerColumnSeparatorWidth"
+| "headerHeight"
+| "iconFontCode"
+| "iconFontCodeGrip"
+| "iconFontCodeLeft"
+| "iconFontCodeRight"
+| "iconFontFamily"
+| "iconFontWeight"
+| "iconSize"
+| "inputFocusBoxShadow"
+| "internalCalculatedLineHeight"
+| "internalPaddedRowHeight"
+| "lineHeight"
+| "listItemHeight"
+| "menuMinWidth"
+| "popupShadow"
+| "rangeSelectionBorderStyle"
+| "rowBorderStyle"
+| "rowBorderWidth"
+| "rowGroupIndentSize"
+| "rowHeight"
+| "selectedTabUnderlineTransitionSpeed"
+| "selectedTabUnderlineWidth"
+| "setFilterIndentSize"
+| "sideBarPanelWidth"
+| "tabMinWidth"
+| "toggleButtonBorderWidth"
+| "toggleButtonHeight"
+| "toggleButtonWidth"
+| "widgetContainerHorizontalPadding"
+| "widgetContainerVerticalPadding"
+| "widgetHorizontalSpacing"
+| "widgetVerticalSpacing"
+| "wrapperBorderRadius"
