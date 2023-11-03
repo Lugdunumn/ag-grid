9933bffd95a619c094484d3e1257c01b400effc9
Bernie Sumption <bernie@berniecode.com>
2023-11-03 08:58:05 +0000

    WIP

--- grid-packages/ag-grid-theme-builder/scripts/utils/scss-ast.test.ts
+++ grid-packages/ag-grid-theme-builder/scripts/utils/scss-ast.test.ts
@@ -76,41 +76,191 @@ test(`Parses ast`, () => {
   `);
 });
 
-test(`Parses ast`, () => {
-  Next up: parse this, and all other @includes in the codebase files below
+test(`Parses include with no arguments`, () => {
   expect(
     parseScssString(`
-    @use "ag";
-
-    el {
-      @include @include ag.keyboard-focus((ag-select-agg-func-virtual-list-item), 1px);
-    }
-  `).nodes,
-  ).toMatchInlineSnapshot();
+      @include ag.foo;
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [],
+        "block": [],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
+  expect(
+    parseScssString(`
+      @include ag.foo();
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [],
+        "block": [],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
 });
 
-// const allFiles = [
-//   'src/internal/base/parts/_reset.scss',
-//   'src/internal/base/parts/_date-time.scss',
-//   'src/internal/base/parts/_advanced-filter.scss',
-//   'src/internal/base/parts/_widgets.scss',
-//   'src/internal/base/parts/_charts.scss',
-//   'src/internal/base/parts/_grid-layout.scss',
-//   'src/internal/base/parts/_common-structural.scss',
-//   'src/internal/base/parts/_filter-tool-panel.scss',
-//   'src/internal/base/parts/_menu.scss',
-//   'src/internal/base/parts/_column-drop.scss',
-//   'src/internal/base/parts/_grid-borders.scss',
-//   'src/internal/base/parts/_print-structural.scss',
-//   'src/internal/base/parts/_sidebar.scss',
-//   'src/internal/base/parts/_columns-tool-panel.scss',
-//   'src/internal/base/parts/_footer.scss',
-//   'src/internal/base/parts/_root.scss',
-//   'src/internal/base/parts/_native-inputs.scss',
-//   'src/internal/base/parts/_icons.scss',
-//   'src/internal/base/parts/_header.scss',
-//   'src/internal/base/_base-variables.scss',
-// ];
+test(`Parses include with value arguments`, () => {
+  expect(
+    parseScssString(`
+      @include ag.foo(a, 3px, -4.5px, calc(var(--foo) * 4));
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [
+          {
+            "type": "value",
+            "value": "a",
+          },
+          {
+            "type": "value",
+            "value": "3px",
+          },
+          {
+            "type": "value",
+            "value": "-4.5px",
+          },
+          {
+            "type": "value",
+            "value": "calc(var(--foo) * 4)",
+          },
+        ],
+        "block": [],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
+});
+
+test(`Parses include with list arguments`, () => {
+  expect(
+    parseScssString(`
+      @include ag.foo((a, foo(), 3px, -10.6, (embedded, list), bar));
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [
+          {
+            "type": "list",
+            "value": [
+              "a",
+              "foo()",
+              "3px",
+              "-10.6",
+              "(embedded, list)",
+              "bar",
+            ],
+          },
+        ],
+        "block": [],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
+});
+
+test(`Parses include with map arguments`, () => {
+  expect(
+    parseScssString(`
+      @include ag.foo((a: b, c: 3px, d: foo(bar), e: -10.5pt));
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [
+          {
+            "type": "map",
+            "value": {
+              "a": "b",
+              "c": "3px",
+              "d": "foo(bar)",
+              "e": "-10.5pt",
+            },
+          },
+        ],
+        "block": [],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
+});
+
+test(`Parses include block argument`, () => {
+  expect(
+    parseScssString(`
+      @include ag.foo() {
+        color: red;
+
+        &.nested {
+          color: green;
+        }
+      };
+    `).nodes,
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "arguments": [],
+        "block": [
+          {
+            "name": "color",
+            "type": "property",
+            "value": "red",
+          },
+          {
+            "children": [
+              {
+                "name": "color",
+                "type": "property",
+                "value": "green",
+              },
+            ],
+            "selectors": [
+              "&.nested",
+            ],
+            "type": "style-rule",
+          },
+        ],
+        "mixin": "ag.foo",
+        "type": "include",
+      },
+    ]
+  `);
+});
+
+const allFiles = [
+  'src/internal/base/parts/_reset.scss',
+  'src/internal/base/parts/_date-time.scss',
+  'src/internal/base/parts/_advanced-filter.scss',
+  'src/internal/base/parts/_widgets.scss',
+  'src/internal/base/parts/_charts.scss',
+  'src/internal/base/parts/_grid-layout.scss',
+  'src/internal/base/parts/_common-structural.scss',
+  'src/internal/base/parts/_filter-tool-panel.scss',
+  'src/internal/base/parts/_menu.scss',
+  'src/internal/base/parts/_column-drop.scss',
+  'src/internal/base/parts/_grid-borders.scss',
+  'src/internal/base/parts/_print-structural.scss',
+  'src/internal/base/parts/_sidebar.scss',
+  'src/internal/base/parts/_columns-tool-panel.scss',
+  'src/internal/base/parts/_footer.scss',
+  'src/internal/base/parts/_root.scss',
+  'src/internal/base/parts/_native-inputs.scss',
+  'src/internal/base/parts/_icons.scss',
+  'src/internal/base/parts/_header.scss',
+  'src/internal/base/_base-variables.scss',
+];
 
 // test(`Parses codebase`, () => {
 //   for (const file of allFiles) {
--- grid-packages/ag-grid-theme-builder/scripts/utils/scss-ast.ts
+++ grid-packages/ag-grid-theme-builder/scripts/utils/scss-ast.ts
@@ -31,7 +31,25 @@ type PropertyDeclaration = {
 type Include = {
   type: 'include';
   mixin: string;
-  argumentMap?: Record<string, string>;
+  arguments: Argument[];
+  block: ParsedNode[];
+};
+
+type Argument = ValueArgument | ListArgument | MapArgument;
+
+type ValueArgument = {
+  type: 'value';
+  value: string;
+};
+
+type ListArgument = {
+  type: 'list';
+  value: string[];
+};
+
+type MapArgument = {
+  type: 'map';
+  value: Record<string, string>;
 };
 
 type LineComment = {
@@ -59,11 +77,8 @@ export const parseScssString = (scssContent: string): FileContent => {
 };
 
 const collectChildren = (output: ParsedNode[], node: Node) => {
-  if (typeof node.value === 'string')
-    throw new Error(
-      `Can't use ${collectChildren.name} on a node with string value ${JSON.stringify(node)}`,
-    );
-  for (const child of node.value) {
+  const nodes = children(node);
+  for (const child of nodes) {
     switch (child.type) {
       case 'space':
         continue;
@@ -80,7 +95,7 @@ const collectChildren = (output: ParsedNode[], node: Node) => {
         collectDeclaration(output, child);
         continue;
       default:
-        logAst(child);
+        console.error(nodes);
         throw new Error(`Don't know how to handle ${child.type} node`);
     }
   }
@@ -93,40 +108,66 @@ const collectAtRule = (output: ParsedNode[], node: Node) => {
       return;
     case '@mixin':
       return collectMixin(output, node);
+    case '@include':
+      return collectInclude(output, node);
     default:
       throw new Error(`Don't know how to handle ${atRule}`);
   }
 };
 
 const collectRule = (output: ParsedNode[], node: Node) => {
-  const selectors = toScss(singleNodeByType(node, 'selector')).split(/\s*,\s*/g);
+  const [selector, block] = splitNodes(node, 'selector', 'block');
   const result: StyleRule = {
     type: 'style-rule',
-    selectors,
+    selectors: toScss(selector).split(/\s*,\s*/g),
     children: [],
   };
-  collectChildren(result.children, singleNodeByType(node, 'block'));
+  collectChildren(result.children, block);
   output.push(result);
 };
 
 const collectDeclaration = (output: ParsedNode[], node: Node) => {
+  const [property, , value] = splitNodes(node, 'property', ':', 'value');
   output.push({
     type: 'property',
-    name: toScss(singleNodeByType(node, 'property')),
-    value: toScss(singleNodeByType(node, 'value')),
+    name: toScss(property),
+    value: toScss(value),
   });
 };
 
-const singleNodeByType = (nodes: Node | Node[], type: string): Node => {
+const splitNodes = (nodes: Node | Node[], ...expected: string[]): Node[] => {
   if (!Array.isArray(nodes)) {
     nodes = children(nodes);
   }
-  const candidates = nodes.filter((node) => node.type === type);
-  if (candidates.length !== 1) {
-    console.error(nodes);
-    throw new Error(`Expected one ${type} node, got ${candidates.length}`);
+  let [expectedType, ...rest] = expected;
+  const result: Node[] = [];
+  for (const node of nodes) {
+    const optional = expectedType?.endsWith('?');
+    if (optional) {
+      expectedType = expectedType.slice(0, -1);
+    }
+    if (node.type === 'space') continue;
+    let expectedValue: string | null = null;
+    if (/[^\w-]/.test(expectedType)) {
+      expectedValue = expectedType;
+      expectedType = 'punctuation';
+    }
+    if (node.type === expectedType && (!expectedValue || expectedValue === node.value)) {
+      result.push(node);
+      [expectedType, ...rest] = rest;
+    } else {
+      if (optional) {
+        result.push(null!);
+      } else {
+        if (node.type === 'punctuation' && (node.value === ';' || node.value === ',')) continue;
+        console.error('Nodes:', nodes);
+        throw new Error(
+          `Expected ${expectedValue || expectedType || 'no more nodes'}, got ${node.type}`,
+        );
+      }
+    }
   }
-  return candidates[0];
+  return result;
 };
 
 const collectComment = (output: ParsedNode[], node: Node) => {
@@ -137,13 +178,129 @@ const collectComment = (output: ParsedNode[], node: Node) => {
 };
 
 const collectMixin = (output: ParsedNode[], node: Node) => {
-  const name = toScss(singleNodeByType(node, 'identifier'));
-  const content = singleNodeByType(node, 'block');
-  if (name === 'output') {
+  const [, name, content] = splitNodes(node, 'atkeyword', 'identifier', 'block');
+  if (name.value === 'output') {
     collectChildren(output, content);
   } else {
     throw new Error(`Don't know how to handle @mixin ${name}`);
   }
+};
+
+const collectInclude = (output: ParsedNode[], node: Node) => {
+  const [, ag, name, args, block] = splitNodes(
+    node,
+    'atkeyword',
+    'identifier',
+    'class',
+    'arguments?',
+    'block?',
+  );
+  const result: Include = {
+    type: 'include',
+    mixin: toScss(ag) + toScss(name),
+    arguments: [],
+    block: [],
+  };
+  if (args) {
+    collectArguments(result.arguments, args);
+  }
+  if (block) {
+    collectChildren(result.block, block);
+  }
+  output.push(result);
+};
+
+const collectArguments = (output: Argument[], argumentsNode: Node) => {
+  const nodes = children(argumentsNode);
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    switch (node.type) {
+      case 'space':
+        continue;
+      case 'punctuation':
+        if (node.value === ',') continue;
+        break;
+      case 'operator':
+      case 'number':
+        let value = toScss(node);
+        while (['identifier', 'number'].includes(nodes[i + 1]?.type)) {
+          // number with units
+          value += toScss(nodes[i + 1]);
+          i++;
+        }
+        output.push({
+          type: 'value',
+          value,
+        });
+        continue;
+      case 'identifier':
+      case 'function':
+        output.push({
+          type: 'value',
+          value: toScss(node),
+        });
+        continue;
+      case 'parentheses':
+        output.push(parseParenthesesArgument(node));
+        continue;
+    }
+    console.error(nodes);
+    throw new Error(`Unexpected ${stringify(node)} in argument list`);
+  }
+};
+
+const parseParenthesesArgument = (node: Node): Argument => {
+  const nodes = children(node);
+  const listItems: ListArgument = {
+    type: 'list',
+    value: [],
+  };
+  const mapItems: MapArgument = {
+    type: 'map',
+    value: {},
+  };
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    switch (node.type) {
+      case 'space':
+        continue;
+      case 'punctuation':
+        if (node.value === ',') continue;
+        break;
+      case 'operator':
+      case 'number':
+        let value = toScss(node);
+        while (['identifier', 'number'].includes(nodes[i + 1]?.type)) {
+          // number with units
+          value += toScss(nodes[i + 1]);
+          i++;
+        }
+        listItems.value.push(value);
+        continue;
+      case 'identifier':
+      case 'function':
+      case 'parentheses':
+        listItems.value.push(toScss(node));
+        continue;
+      case 'declaration':
+        const [declName, , declValue] = splitNodes(node, 'property', ':', 'value');
+        mapItems.value[toScss(declName)] = toScss(declValue);
+        continue;
+    }
+    console.error(nodes);
+    throw new Error(`Unexpected ${stringify(node)} in argument list`);
+  }
+  const isMap = Object.keys(mapItems.value).length > 0;
+  if (listItems.value.length > 0 && isMap) {
+    console.error(nodes);
+    throw new Error(
+      `Arguments has both map and list kinds: ${JSON.stringify({
+        list: listItems.value,
+        map: mapItems.value,
+      })}`,
+    );
+  }
+  return isMap ? mapItems : listItems;
 };
 
 const children = (node: Node) => {
